5c1cc02af1215512ab76aa5d6827495a56c034f4
MohRizal <rizaladventure07@gmail.com>
2026-05-13 13:38:51 +0700

    fix: add ssl connection for aiven

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,14 +1,11 @@
-const mysql = require('mysql2/promise');
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'surat_desa',
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  // TAMBAHKAN BAGIAN INI WAJIB:
   ssl: {
     rejectUnauthorized: false
-  },
-  waitForConnections: true, connectionLimit: 10, timezone: '+07:00'
+  }
 });
-pool.getConnection().then(c=>{console.log('DB terhubung');c.release();}).catch(e=>console.error('DB Error:',e.message));
-module.exports = pool;
